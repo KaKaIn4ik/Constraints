0f1190c7ce049d75d7c07f5da3b19e25d4dbbda1
EduardKungurtsev <e.kungurtsev@2gis.ru>
2022-03-03 20:12:55 +0700

    Initial Commit

---
+++ Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 03.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
